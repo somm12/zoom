580f653224f40b519eb992ab0ea379bbca48a23c
somm12 <sominan8744@gmail.com>
2022-06-25 16:38:24 +0900

    connect frontend and backend with websocket server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
-alert("hello");
+const socket = new WebSocket(`ws://${window.location.host}`);
+// 여기서 socket은 서버로의 연결을 뜻한다.
+socket.addEventListener("open", () => {
+  console.log("connected to Server✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("Just got this", message.data, "from the server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("disconnected to Server❌");
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,5 +11,18 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
+// http와 ws 서버 둘다 작동.
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+// http server 위에 websocket 서버를 올림 => localhost가 동일 포트에서 http, ws request 모두 처리할 수 있다.
+
+// 여기서 socket은 연결된 브라우저를 뜻한다.
+wss.on("connection", (socket) => {
+  socket.send("hello!!");
+  console.log("connected to Browser✅");
+});
+
+server.listen(3000, handleListen);
